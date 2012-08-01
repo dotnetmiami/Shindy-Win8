7027f6a6dbf9ad16b167da579cd704124c98faf6
Richie Rump <jorriss@gmail.com>
2012-07-31 23:58:15 -0400

    Added console arguments to ShindyDataLoader
    Console arguments were added to ShindyDataLoader.
    Renamed data loader to ShindyDataLoader.
    Added the ability to load JSON files from disk to RavenDB.

---
+++ ShindyDataLoader/Options.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using CommandLine.Text;
+
+namespace EventTestConsole
+{
+    class Options
+    {
+        [Option("r", "ravenurl", Required = false, HelpText = "The URL for RavenDB.")]
+        public string RavenURL { get; set; }
+
+        [Option("j", "jsonpath", Required = false, HelpText = "The URL or the path of the JSON file to be loaded.")]
+        public string JSONPath { get; set; }
+
+        [Option("d", "dbname", Required = false, HelpText = "The name of the Raven database.")]
+        public string DBName { get; set; }
+
+    }
+}
