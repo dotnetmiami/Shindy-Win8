d6ecf33e5caad3236e7df09ec14cd36417088c79
Richie Rump <jorriss@gmail.com>
2012-08-21 00:13:29 -0400

    Added Nancy Unit Test Framework

--- ShindyTest/ShindyWebService.Test/EventsModuleTest.cs
+++ ShindyTest/ShindyWebService.Test/EventsModuleTest.cs
@@ -10,6 +10,7 @@ using Nancy.Json;
 using Nancy.Testing;
 using Moq;
 using Xunit;
+using Newtonsoft.Json;
 
 namespace EventTest.ShindyWebService
 {
@@ -17,11 +18,14 @@ namespace EventTest.ShindyWebService
     {
         private Browser app;
         private Mock<EventLibrary.IEventsSvcBroker> eventBroker;
+        private IEnumerable<Event> eventData;
 
         public EventsModuleTest()
         {
+            eventData = GetEventData();
+
             eventBroker = new Mock<EventLibrary.IEventsSvcBroker>();
-            eventBroker.Setup(foo => foo.GetEvents(It.IsAny<int>(), It.IsAny<int>())).Returns(EventData());
+            eventBroker.Setup(foo => foo.GetEvents(It.IsAny<int>(), It.IsAny<int>())).Returns(eventData);
             var bootstrapper = new ConfigurableBootstrapper(with =>
             {
                 with.Module<EventsModule>().Dependency(eventBroker.Object);
@@ -29,27 +33,57 @@ namespace EventTest.ShindyWebService
             app = new Browser(bootstrapper);
         }
 
-        public IEnumerable<Event> EventData()
+        public IEnumerable<Event> GetEventData()
         {
+            var group1 = new Group { Id = "groups/1", Name="Group One", IsExternalGroup = false };
+            var event1 = new Event { Id = "events/1", Title = "Testing Events One", EventDateTime = TimeZoneInfo.ConvertTimeToUtc(DateTime.Now.AddDays(-15), TimeZoneInfo.Local) };
+            event1.HostedGroups.Add(group1);
+            var event2 = new Event { Id = "events/2", Title = "Testing Events Two", EventDateTime = TimeZoneInfo.ConvertTimeToUtc(DateTime.Now.AddDays(15), TimeZoneInfo.Local) };
+            event2.HostedGroups.Add(group1);
+            var event3 = new Event { Id = "events/3", Title = "Testing Events Three", EventDateTime = TimeZoneInfo.ConvertTimeToUtc(DateTime.Now.AddDays(45), TimeZoneInfo.Local) };
+            event3.HostedGroups.Add(group1);
 
-            IEnumerable<Event> returnEvent = new[] { new Event { Id = "event/1", Title = "Testing Events", EventDateTime = DateTime.Now } }; 
-            return returnEvent;
+            List<Event> returnEvent = new List<Event>();
+            returnEvent.Add(event1);
+            returnEvent.Add(event2);
+            returnEvent.Add(event3);
+            return returnEvent.AsEnumerable<Event>();
         }
 
         // Test naming pattern: [MethodName]_[StateUnderTest]_[ExpectedBehavior].
 
         [Fact]
-        public void EventModule_DefaultParams_ValidEvents()
+        public void EventModule_DefaultParams_GetEventCalled()
         {
             var response = app.Get("/events/",
-                 with => {
+                 with =>
+                 {
                      with.HttpRequest();
                  });
 
-            //Then
             Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
-            //var eventTest = response.Body.DeserializeJson<EventLibrary.Entities.Event>();
-            
+            Assert.Contains("events/1", response.Body.AsString());
+            eventBroker.Verify(m => m.GetEvents(10, 1));
+        }
+
+        [Fact]
+        public void EventModule_PageNumberPageSize_GetEventCalled()
+        {
+            var response = app.Get("/events/",
+                 with =>
+                 {
+                     with.HttpRequest();
+                     with.Query("pagenumber", "1");
+                     with.Query("pagesize", "2");
+                 });
+
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetEvents(2, 1));
         }
 
     }
