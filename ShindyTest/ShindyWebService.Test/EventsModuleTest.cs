29194c16401cb7e1685535d91735dd35b5b641f9
Richie Rump <jorriss@gmail.com>
2012-08-24 17:55:16 -0400

    Completed unit tests to event web service.

--- ShindyTest/ShindyWebService.Test/EventsModuleTest.cs
+++ ShindyTest/ShindyWebService.Test/EventsModuleTest.cs
@@ -26,8 +26,11 @@ namespace EventTest.ShindyWebService
 
             eventBroker = new Mock<EventLibrary.IEventsSvcBroker>();
             eventBroker.Setup(foo => foo.GetEvents(It.IsAny<int>(), It.IsAny<int>())).Returns(eventData);
+            eventBroker.Setup(foo => foo.GetEvents(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<bool>())).Returns(eventData);
             eventBroker.Setup(foo => foo.GetUpcomingEvents(It.IsAny<int>(), It.IsAny<int>())).Returns(eventData);
             eventBroker.Setup(foo => foo.GetUpcomingEvents(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<bool>())).Returns(eventData);
+            eventBroker.Setup(foo => foo.GetPreviousEvents(It.IsAny<int>(), It.IsAny<int>())).Returns(eventData);
+            eventBroker.Setup(foo => foo.GetPreviousEvents(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<bool>())).Returns(eventData);
             var bootstrapper = new ConfigurableBootstrapper(with =>
             {
                 with.Module<EventsModule>().Dependency(eventBroker.Object);
@@ -75,6 +78,7 @@ namespace EventTest.ShindyWebService
         }
 
         // Test naming pattern: [MethodName]_[StateUnderTest]_[ExpectedBehavior].
+        #region /events
 
         [Fact]
         public void EventModule_DefaultParams_GetEventCalled()
@@ -114,6 +118,10 @@ namespace EventTest.ShindyWebService
             eventBroker.Verify(m => m.GetEvents(10, 1));
         }
 
+        #endregion
+
+        #region /events/upcoming
+
         [Fact]
         public void EventModule_UpcomingDefaultParams_GetEventCalled()
         {
@@ -152,6 +160,10 @@ namespace EventTest.ShindyWebService
             eventBroker.Verify(m => m.GetUpcomingEvents(10, 1));
         }
 
+        #endregion
+
+        #region /events/{groupname}/upcoming
+
         [Fact]
         public void EventModule_GroupUpcomingDefaultParams_GetEventCalled()
         {
@@ -176,6 +188,273 @@ namespace EventTest.ShindyWebService
             Assert.Contains("events/2", response.Body.AsString());
             eventBroker.Verify(m => m.GetUpcomingEvents("dotnet miami", 2, 1, false));
         }
+
+        [Fact]
+        public void EventModule_GroupUpcomingInvalidPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/upcoming", "abcd", "efgh");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetUpcomingEvents("dotnet miami", 10, 1, false));
+        }
+
+        #endregion
+
+        #region /events/{groupname}/upcoming/external
+
+        [Fact]
+        public void EventModule_GroupUpcomingExternalDefaultParams_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/upcoming/external");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            eventBroker.Verify(m => m.GetUpcomingEvents("dotnet miami", 10, 1, true));
+        }
+
+        [Fact]
+        public void EventModule_GroupUpcomingExternalPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/upcoming/external", "2", "1");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetUpcomingEvents("dotnet miami", 2, 1, true));
+        }
+
+        [Fact]
+        public void EventModule_GroupUpcomingExternalInvalidPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/upcoming/external", "abcd", "efgh");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetUpcomingEvents("dotnet miami", 10, 1, true));
+        }
+
+        #endregion
+
+        #region /events/{groupname}
+
+        [Fact]
+        public void EventModule_GroupDefaultParams_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            eventBroker.Verify(m => m.GetEvents("dotnet miami", 10, 1, false));
+        }
+
+        [Fact]
+        public void EventModule_GroupPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/", "2", "1");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetEvents("dotnet miami", 2, 1, false));
+        }
+
+        [Fact]
+        public void EventModule_GroupInvalidPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/", "abcd", "efgh");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetEvents("dotnet miami", 10, 1, false));
+        }
+
+        #endregion
+
+        #region /events/{groupname}/external
+
+        [Fact]
+        public void EventModule_GroupExternalDefaultParams_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/external");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            eventBroker.Verify(m => m.GetEvents("dotnet miami", 10, 1, true));
+        }
+
+        [Fact]
+        public void EventModule_GroupExternalPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/external", "2", "1");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetEvents("dotnet miami", 2, 1, true));
+        }
+
+        [Fact]
+        public void EventModule_GroupExternalInvalidPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/external", "abcd", "efgh");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetEvents("dotnet miami", 10, 1, true));
+        }
+
+        #endregion
+
+        #region /events/previous
+
+        [Fact]
+        public void EventModule_PreviouslDefaultParams_GetEventCalled()
+        {
+            var response = CallWebService("/events/previous");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            eventBroker.Verify(m => m.GetPreviousEvents(10, 1));
+        }
+
+        [Fact]
+        public void EventModule_PreviousPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/previous", "2", "1");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetPreviousEvents(2, 1));
+        }
+
+        [Fact]
+        public void EventModule_PreviousInvalidPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/previous", "abcd", "efgh");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetPreviousEvents(10, 1));
+        }
+
+        #endregion
+
+        #region /events/{groupname}/previous
+
+        [Fact]
+        public void EventModule_GroupPreviouslDefaultParams_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/previous");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            eventBroker.Verify(m => m.GetPreviousEvents("dotnet miami", 10, 1, false));
+        }
+
+        [Fact]
+        public void EventModule_GroupPreviousPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/previous", "2", "1");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetPreviousEvents("dotnet miami", 2, 1, false));
+        }
+
+        [Fact]
+        public void EventModule_GroupPreviousInvalidPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/previous", "abcd", "efgh");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetPreviousEvents("dotnet miami", 10, 1, false));
+        }
+
+        #endregion
+
+        #region /events/{groupname}/previous/external
+
+        [Fact]
+        public void EventModule_GroupPreviousExternallDefaultParams_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/previous/external");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            eventBroker.Verify(m => m.GetPreviousEvents("dotnet miami", 10, 1, true));
+        }
+
+        [Fact]
+        public void EventModule_GroupPreviousExternalPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/previous/external", "2", "1");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetPreviousEvents("dotnet miami", 2, 1, true));
+        }
+
+        [Fact]
+        public void EventModule_GroupPreviousExternalInvalidPageNumberPageSize_GetEventCalled()
+        {
+            var response = CallWebService("/events/dotnet miami/previous/external", "abcd", "efgh");
+            var events = response.Body.DeserializeJson<IEnumerable<Event>>();
+
+            Assert.Equal(Nancy.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(events.Count(), eventData.Count());
+            Assert.Contains("events/1", response.Body.AsString());
+            Assert.Contains("events/2", response.Body.AsString());
+            eventBroker.Verify(m => m.GetPreviousEvents("dotnet miami", 10, 1, true));
+        }
+
+        #endregion
 
     }
 }
